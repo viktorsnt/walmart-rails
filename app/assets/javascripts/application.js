38492746b9fcc0524a9e777d31ce1d1529dadaa1
Victor Almeida <viktorsnt@gmail.com>
2016-08-04 16:39:59 -0300

    front end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,35 @@
 //= require turbolinks
 //= require react_ujs
 //= require components
+$(document).ready(function(){
+
+  $('input[type="number"]').each(function(){
+    var self = $(this);
+    function update_quantity() {
+      $.ajax({
+        url: self.parents('form').attr('action'),
+        data: {
+          cart_item: {
+            quantity: self.val()
+          }
+        },
+        method: 'PUT',
+        success: function(data){
+          console.log(data)
+          $('#cart_item_total_'+data.cart_item.product_id).html('R$'+data.cart_item.total);
+          $('#subtotal').html('R$'+data.cart.subtotal);
+          $('#total').html('R$'+data.cart.total);
+        }
+      })
+    }
+    self.prev().on('click',function(e) {
+      if (self.val() > 1) self.val( parseInt(self.val()) - 1 );
+      update_quantity()
+    })
+    self.next().on('click',function(e) {
+      self.val( parseInt(self.val()) + 1 );
+      update_quantity()
+    })
+  })
+
+})
